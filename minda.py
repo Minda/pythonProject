0c624e050c46c88c3762d2a05b99a01c8cda723e
Minda Myers <kittentech@gmail.com>
2023-11-21 14:49:17 -0800

    unstable: last attempt to get random to work on GPU

--- minda.py
+++ minda.py
@@ -1,9 +1,9 @@
 import os
 
 # needs to appear before `from numba import cuda`
-# os.environ["NUMBA_ENABLE_CUDASIM"] = "1"
+os.environ["NUMBA_ENABLE_CUDASIM"] = "1"
 # set to "1" for more debugging, but slower performance
-# os.environ["NUMBA_CUDA_DEBUGINFO"] = "1"
+os.environ["NUMBA_CUDA_DEBUGINFO"] = "1"
 
 from numba import cuda
 from numba.cuda.random import create_xoroshiro128p_states, xoroshiro128p_uniform_float32
@@ -637,15 +637,17 @@ def find_min(centroids):
 def get_random_index(input, rng_states):
   seed = cuda.threadIdx.x
   row = cuda.blockIdx.x
-  row_length = input.shape[0]
+  row_length = input.shape[0] - 1
   random_index = int(LINE_SIZE/2)
 
 
   # Generate a random float in the range [0, 1)
-  rand =  random_index#xoroshiro128p_uniform_float32(rng_states, seed)
+  rand = xoroshiro128p_uniform_float32(rng_states, seed)
+  #print("Random Number: ", rand)
 
   # Transform the float to an int in the range of the row length
-  #random_index = int(rand * row_length)
+  random_index = int(rand * row_length)
+  #print("Random Index: ", random_index)
 
   return random_index
 
@@ -1111,7 +1113,7 @@ if __name__ == "__main__":
 
   new_code_stats = TimingStats()
   # Create an array of RNG states
-  rng_states = cuda.random.create_xoroshiro128p_states(NUM_SEEDS, seed=0)
+  rng_states = cuda.random.create_xoroshiro128p_states(NUM_SEEDS*LINE_SIZE, seed=1)
 
   print("random numbers. ", rng_states)
 
