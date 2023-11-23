ab79381bc89317992ac524f597faa0de2c53c36f
Minda Myers <kittentech@gmail.com>
2023-11-22 16:18:26 -0800

    stable but very slow

--- main.py
+++ main.py
@@ -1,31 +1,25 @@
 import os
-
-# needs to appear before `from numba import cuda`
-os.environ["NUMBA_ENABLE_CUDASIM"] = "1"
-# set to "1" for more debugging, but slower performance
-os.environ["NUMBA_CUDA_DEBUGINFO"] = "1"
-
-from numba import cuda
-import numba
-
-print(numba.__version__)
-from numba.cuda.random import create_xoroshiro128p_states, xoroshiro128p_uniform_float32
 import numpy as np
 import math
 import time
+from random import randint
+from pdb import set_trace
 
+os.environ["OMP_NUM_THREADS"] = "1"
+# os.environ["NUMBA_ENABLE_CUDASIM"] = "1"  # needs to appear before `from numba import cuda`
+# os.environ["NUMBA_CUDA_DEBUGINFO"] = "1"  # set to "1" for more debugging, but slower performance
+
+from numba import cuda, int32, int64
 from sklearn.cluster import KMeans
-from sklearn.metrics import adjusted_rand_score
 
-NUM_ROWS = 1
-NUM_ITERATIONS = 2
-TEST_ITERATIONS = 1
-LINE_SIZE = 20
+NUM_ROWS = 10
+NUM_ITERATIONS = 4
+TEST_ITERATIONS = 4
+LINE_SIZE = 100
 NUM_CLUSTERS = 3
 MAX_INPUT_VALUE = 100
 NUM_SEEDS = 32
-EPSILON_PERCENT = 0.002
-
+EPSILON_PERCENT = 0.02
 
 class TimingStats:
     def __init__(self):
@@ -76,7 +70,6 @@ class TimingStats:
 @cuda.jit(device=True)
 def copy_shared_float(shared_arr, device_arr):
     shared_arr_len = shared_arr.shape[0]
-    device_arr_len = device_arr.shape[0]
     for s_index in range(shared_arr_len):
         device_arr[s_index] = np.float32(shared_arr[s_index])
 
@@ -84,7 +77,6 @@ def copy_shared_float(shared_arr, device_arr):
 @cuda.jit(device=True)
 def copy_shared_int(shared_arr, device_arr):
     shared_arr_len = shared_arr.shape[0]
-    device_arr_len = device_arr.shape[0]
     for s_index in range(shared_arr_len):
         device_arr[s_index] = np.int32(shared_arr[s_index])
 
@@ -99,7 +91,6 @@ def fill_row(input, shared_input):
 @cuda.jit(device=True)
 def find_max(centroids):
     res = -1.0
-    index = 0
     for centroid in centroids:
         if res == -1.0 or res < centroid:
             res = centroid
@@ -128,29 +119,26 @@ def find_min_index(error_array):
 
 @cuda.jit(device=True)
 def find_yard_stick(centroids):
-    min = MAX_INPUT_VALUE
-    yard_stick = prev_centroid = None
+    mini = MAX_INPUT_VALUE
+    prev_centroid = None
 
     # first centroid
     if len(centroids) > 0:
-        prev_centroid = min = centroids[0]
+        prev_centroid = mini = centroids[0]
 
     # compare remaining centroids
     for centroid in centroids[1:]:
-        distance = centroid - prev_centroid
-        if distance < min:
-            min = distance
+        distance = abs(centroid - prev_centroid)
+        if distance < mini:
+            mini = distance
         prev_centroid = centroid
 
-    yard_stick = min
+    yard_stick = mini
     return yard_stick  # return a float
 
 
 @cuda.jit(device=True)
 def sort_centroids(centroids):
-    row = cuda.blockIdx.x
-    seed = cuda.threadIdx.x
-
     if centroids.ndim < 1:  # ensure we are in-bounds
         return
 
@@ -174,11 +162,11 @@ def converged(centroids, old_centroids, epsilon):
 
     if centroids_length != old_centroids_length:
         return
-    # TODO: Check if creating centroid is automatically freed. This should be true.
+# TODO: Check if creating centroid is automatically freed. This should be true.
     for centroid_index in range(centroids_length):
         centroid = centroids[centroid_index]
         old_centroid = old_centroids[centroid_index]
-        if epsilon < abs_sub(centroid, old_centroid):
+        if epsilon < abs(centroid - old_centroid):
             return False
 
     return True  # return a boolean
@@ -188,7 +176,7 @@ def converged(centroids, old_centroids, epsilon):
 # is called one time for each centroid that needs to be selected from the dataset
 @cuda.jit(device=True)
 def largest_smallest_distance(line, centroids, iter):  # row_data, seed_centroids, centroid_index
-    min = 0
+    mini = 0
     index = 0
     if iter > centroids.shape[0]:  # iter must be in range to execute rest of code
         return
@@ -198,42 +186,16 @@ def largest_smallest_distance(line, centroids, iter):  # row_data, seed_centroid
         for j in range(iter):  # loop only up until our current centroid
             row_element = line[i]
             centroid = centroids[j]
-            distance = abs_sub(row_element, centroid)
+            distance = abs(row_element - centroid)
             if distance < min_distance:
                 min_distance = distance
-        if min_distance > min:
-            min = min_distance
+        if min_distance > mini:
+            mini = min_distance
             index = i
     centroids[iter] = line[index]
 
-
-# This function subtracts two values and returns an absolute value of the difference
 @cuda.jit(device=True)
-def abs_sub(val1, val2):
-    diff = val2 - val1
-    if diff >= 0:
-        return diff
-    return -diff
-
-
-@cuda.jit(device=True)
-def get_random_index(input, rng_states):
-    seed = cuda.threadIdx.x
-    row = cuda.blockIdx.x
-    row_length = input.shape[0]
-    random_index = -1
-
-    # Generate a random float in the range [0, 1)
-    rand = 1 #cuda.random.xoroshiro128p_uniform_float32(rng_states, seed * row)
-
-    # Transform the float to an int in the range of the row length
-    random_index = int(rand * row_length)
-
-    return random_index
-
-
-@cuda.jit(device=True)
-def get_initial_centroids(row_data, seed_centroids, rng_states):
+def get_initial_centroids(row_data, seed_centroids):
     # remember, seed_centroids is of size [NUM_CLUSTERS]
     row = cuda.blockIdx.x
     seed = cuda.threadIdx.x
@@ -242,11 +204,10 @@ def get_initial_centroids(row_data, seed_centroids, rng_states):
     if row_data.ndim < 1 or seed_centroids.ndim < 1:
         return
 
-    row_length = row_data.shape[0]
     centroids_length = seed_centroids.shape[0]
 
     # select a first centroid from the row data at random
-    first_centroid_index = get_random_index(row_data, rng_states)
+    first_centroid_index = np.int32(seed) * 3
 
     # get the furthest distance from the random centroid for remaining centroids
     for centroid_index in range(centroids_length):
@@ -273,11 +234,11 @@ def get_labels(labels, row_data, centroids):
     for row_index in range(row_data_length):
         element = row_data[row_index]
         best_centroid_index = 0
-        best_distance = abs_sub(element, centroids[best_centroid_index])
+        best_distance = abs(element - centroids[best_centroid_index])
         labels[row_index] = int(element)
         for centroid_index in range(centroids_length):
             centroid = centroids[centroid_index]
-            curr_dist = abs_sub(element, centroid)
+            curr_dist = abs(element - centroid)
             if curr_dist < best_distance:
                 best_centroid_index = centroid_index
                 best_distance = curr_dist
@@ -292,26 +253,11 @@ def fill_ones(arr):
 
 @cuda.jit(device=True)
 def get_centroids(row_data, labels, centroids):
-    row = cuda.blockIdx.x
-    seed = cuda.threadIdx.x
-
-    '''on the first call, get centroids will be called on the inital centroids
-  we selected. Then we need to re-compute the centroids based on the row data.
-  Then '''
-
     if row_data.ndim < 1 or labels.ndim < 1 or centroids.ndim < 1:
         return
 
     labels_length = labels.shape[0]
-    row_data_length = row_data.shape[0]
     centroids_length = centroids.shape[0]
-
-    num_mapped = 0
-    # num_of_data_for_centroid = cuda.shared.array(shape=(5), dtype=np.float32)
-    # shared_error = cuda.shared.array(shape=(NUM_SEEDS), dtype=np.float32)
-
-    if (row_data_length != labels_length):  # ensure we stay in range for assigments
-        return
 
     for centroids_idx in range(centroids_length):
         count = 0
@@ -343,7 +289,7 @@ def calc_error(centroids, row_data, labels):
         for j in range(LINE_SIZE):
             data = row_data[j]
             if labels[j] == i:
-                diff = abs_sub(data, centroid)
+                diff = abs(data - centroid)
                 sse += diff * diff
     if sse == 0:
         return math.inf
@@ -352,10 +298,10 @@ def calc_error(centroids, row_data, labels):
 
 @cuda.jit(device=True)
 def kmeans(input, output_labels, output_centroids,
-           rng_states):  # these are already shared memory and a single row for each
+    ):  # these are already shared memory and a single row for each
     seed = cuda.threadIdx.x
 
-    get_initial_centroids(input, output_centroids, rng_states)
+    get_initial_centroids(input, output_centroids)
 
     sort_centroids(output_centroids)
 
@@ -378,12 +324,12 @@ def kmeans(input, output_labels, output_centroids,
 
 
 @cuda.jit()
-def cuda_kmeans(input, output_labels, output_centroids, rng_states):
+def cuda_kmeans(input, output_labels, output_centroids):
     row = cuda.blockIdx.x
     seed = cuda.threadIdx.x
 
-    if row == 1 and seed == 1:
-        from pdb import set_trace; set_trace()
+    if hasattr(input, "ndim") and input.ndim > 1:
+        row_data_length = input.shape[1]
 
     # Shared memory should only run once per block, 2d arrays have shape NUM ROWS x NUM COLS
     shared_input = cuda.shared.array(shape=(LINE_SIZE,), dtype=np.float32)
@@ -395,7 +341,7 @@ def cuda_kmeans(input, output_labels, output_centroids, rng_states):
     fill_row(input[row], shared_input)
 
     # Run kmeans to convergence on row and seed
-    kmeans(shared_input, shared_labels[seed], shared_centroids[seed], rng_states)
+    kmeans(shared_input, shared_labels[seed], shared_centroids[seed])
 
     # TODO: add error calculation back in
     shared_error[seed] = calc_error(shared_centroids[seed], shared_input, shared_labels[seed])
@@ -411,26 +357,9 @@ def cuda_kmeans(input, output_labels, output_centroids, rng_states):
         copy_shared_float(shared_centroids[min], output_centroids[row])
 
 
-def compare_clusters(labels1, labels2):
-    """
-    Compare two clustering label sets using the Adjusted Rand Index (ARI).
-
-    Parameters:
-    labels1 (array-like): The first set of clustering labels.
-    labels2 (array-like): The second set of clustering labels.
-
-    Returns:
-    float: The ARI score, a value between -1 and 1. A score close to 1 indicates
-           a high similarity between the two label sets, while a score close to 0
-           or negative indicates a low similarity.
-    """
-    ari_score = adjusted_rand_score(labels1, labels2)
-    return ari_score
-
-
 # Host code:
 # Set up all host code to run kmeans
-def test_new_kmeans(input_data, new_code_stats, rng_states, printouts=True):
+def test_new_kmeans(input_data, new_code_stats, printouts=True):
     results_host = []
     centroids_arr = []
     try:
@@ -455,7 +384,8 @@ def test_new_kmeans(input_data, new_code_stats, rng_states, printouts=True):
 
         # number of rows, number of seeds
         start = time.time()
-        cuda_kmeans[(NUM_ROWS,), (NUM_SEEDS,)](input_rows, output_labels, device_centroids, rng_states)
+        cuda.synchronize()
+        cuda_kmeans[(NUM_ROWS,), (NUM_SEEDS,)](input_rows, output_labels, device_centroids)
         cuda.synchronize()
         end = time.time()
         new_code_stats.add_runtime(end - start)
@@ -464,22 +394,18 @@ def test_new_kmeans(input_data, new_code_stats, rng_states, printouts=True):
         centroids_arr = np.round(device_centroids.copy_to_host(), 2)
 
         if printouts:
-            print("Centroids after: \n", np.round(centroids_arr[:2, :], 2))
-            print("Labels after:, \n", results_host[:2, :])
+            print("Centroids after: \n", np.round(centroids_arr[:5, :], 2))
+            print("Labels after: \n", results_host[:5, :])
         print("\n \n \n")
 
         # SKLEARN KMEANS
-        ari_array = []
-        #for i in range(NUM_ROWS):
-        #    if i > 5:
-        #        break
-        #    output1 = KMeans(n_clusters=3, random_state=0, n_init="auto").fit((input_data[i]).reshape(-1, 1))
-        #    output2 = KMeans(n_clusters=3, random_state=0, n_init="auto").fit((input_data[i]).reshape(-1, 1))
-        #    sklearn_output_centroids1 = np.sort(output1.cluster_centers_.flatten())
-        #    error_array = np.linalg.norm(
-        #        np.array(centroids_arr[i:i + 1, :].flatten()) - np.array(sklearn_output_centroids1))
-        #    print(
-        #        f"SKLearn kmeans row {i}: {sklearn_output_centroids1},  {centroids_arr[i:i + 1, :].flatten()}, {error_array}")
+        # for i in range(NUM_ROWS):
+        #     if i > 5:
+        #         break
+        #     output1 = KMeans(n_clusters=3, random_state=0, n_init="auto").fit((input_data[i]).reshape(-1, 1))
+        #     sklearn_output_centroids1 = np.sort(output1.cluster_centers_.flatten())
+        #     error_array = np.linalg.norm(np.array(centroids_arr[i:i+1, :].flatten()) - np.array(sklearn_output_centroids1))
+        #     print(f"SKLearn kmeans row {i}: {sklearn_output_centroids1},  {centroids_arr[i:i+1, :].flatten()}, {error_array}")
 
     except Exception as e:
         print("An error occurred:", str(e))
@@ -494,15 +420,11 @@ if __name__ == "__main__":
         raise ValueError("Invalid number of clusters. k must be greater than 0.")
 
     new_code_stats = TimingStats()
-    # Create an array of RNG states
-    rng_states = [[1],[2]]# cuda.random.create_xoroshiro128p_states(LINE_SIZE, seed=0)
 
     for test_iteration in range(TEST_ITERATIONS):
         # Generate some fake input data
         input_data = np.random.rand(NUM_ROWS, LINE_SIZE).astype(np.float32) * (MAX_INPUT_VALUE)
-        labels, centroids_arr = test_new_kmeans(input_data, new_code_stats,
-                                                rng_states, True)
-
+        labels, centroids_arr = test_new_kmeans(input_data, new_code_stats, True)
     # Report on Runtime Statistics
     print("\n---NEW CODE---\n")
     new_code_stats.print_report()
@@ -512,5 +434,5 @@ if __name__ == "__main__":
 
     grid_dim = NUM_ROWS
     block_dim = NUM_SEEDS
-    # test_components(grid_dim, block_dim, rng_states)
-    # test_new_code(grid_dim, block_dim, rng_states)
+    # test_components(grid_dim, block_dim)
+    # test_new_code(grid_dim, block_dim)
